b47bbb8f6ae9fbe176c5f2459d0592debed365c9
myohtetaung7546 <myohtet2099@gmail.com>
2022-09-07 10:36:49 +0630

    initial commit

---
+++ Chorus.java
@@ -0,0 +1,12 @@
+public class Chorus {
+    public static void main(String[] args) {
+        singChorus();
+    }
+
+    public void singChorus() {
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on thes moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+    }
+}
